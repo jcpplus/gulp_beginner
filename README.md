6650963800a5e49d04aa8483f82e20ccd84b764b
Jesse.Liang <jesse.liang.suryani@suryani.cn>
2014-11-28 17:06:49 +0800

    add files

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ gulp_beginner
 =============
 
 a gulp file demo
+
+##### plugins
+- gulp-sass
+- gulp-autoprefixer
+- gulp-sourcemaps
+
+##### thanks 
+- [Getting Gulp Plugins To Play Nice With SCSS and Sourcemaps](http://www.devworkflows.com/posts/getting-scss-auto-prefixer-and-source-map-to-play-nice/) 
+- [Introduction to Gulp.js 4: Creating CSS with Sass (and Compass)](http://stefanimhoff.de/2014/gulp-tutorial-4-css-generation-sass/)
---
+++ assets/gulpfile.js
@@ -0,0 +1,98 @@
+/*!
+ * gulp
+ * $ npm install gulp-sass gulp-autoprefixer gulp-sourcemaps gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache del --save-dev
+ */
+ 
+// Load plugins
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    clean = require('gulp-clean'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+ 
+// Styles
+gulp.task('styles', function() {
+  return gulp.src('sass/*.scss')
+    .pipe(sass({ sourcemap: true}))
+
+    .on('error', function (error) {
+            console.error(error);
+            this.emit('end');
+     })
+
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(autoprefixer({browsers: ['last 2 versions']}))
+    .pipe(sourcemaps.write('./maps'))
+
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+ 
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src('js/**/*.js')
+    .pipe(jshint('js/.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+ 
+// Images
+gulp.task('images', function() {
+  return gulp.src('img/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+ 
+// Clean del
+gulp.task('del', function(cb) {
+    del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], cb)
+});
+
+// clean? 
+gulp.task('clean', function() {  
+  return gulp.src(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], {read: false})
+    .pipe(clean());
+});
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+ 
+// Watch
+gulp.task('watch', function() {
+ 
+  // Watch .scss files
+  gulp.watch('scss/**/*.scss', ['styles']);
+ 
+  // Watch .js files
+  gulp.watch('js/**/*.js', ['scripts']);
+ 
+  // Watch image files
+  gulp.watch('img/**/*', ['images']);
+ 
+  // Create LiveReload server
+  livereload.listen();
+ 
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', livereload.changed);
+ 
+});
